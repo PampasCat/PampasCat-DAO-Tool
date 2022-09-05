14ff865e15c038d7648456b4f9703ea1e40acd91
PampasCat <100556153+PampasCat@users.noreply.github.com>
2022-09-05 16:32:59 +0800

    Update pampascat.sol

--- contracts/pampascat/pampascat.sol
+++ contracts/pampascat/pampascat.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@solmate/src/utils/FixedPointMathLib.sol";
-
 error AllocationCannotBeZero();
 error AlreadyDeposited();
 error DuplicatePartner();
